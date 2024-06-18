afedc923ad603496d4412ce68a381cbd32686eec
Afifrrahman <115561546+Afifrrahman@users.noreply.github.com>
2024-06-18 14:36:04 +0700

    Perbaikan Beckend

--- Backend/src/controller/giziController.js
+++ Backend/src/controller/giziController.js
@@ -2,7 +2,7 @@ const Gizi = require('../models/giziModels');
 
 const responseSucces = (res, message, data)=> {
   res.status(200).json({
-    status: 'success',
+    status: 'succes',
     message: message,
     data: data
   });
@@ -15,7 +15,7 @@ const responseError = (res, statusCode, message)=> {
   });
 };
 
-const getAllGizi = (req, res) => {
+const getAllGizi = (req,res)=> {
   Gizi.getAll((err, results)=> {
     if(err) {
       responseError(res, 500, err.message);
@@ -27,67 +27,18 @@ const getAllGizi = (req, res) => {
 
 const getGiziById = (req, res)=> {
   const id = req.params.id;
-  Gizi.getById(id, (err, results)=> {
-    if (err){
-      responseError(res, 500, err.message);
-    }else if (!results) {
-      responseError(res, 404, 'Data Gizi tidak ditemukan!!');
-    } else {
-      responseSucces(res, `Data gizi dengan ID ${id} berhasil ditemukan`, results);
-    }
-  });
-};
-
-const createGizi = (req, res)=> {
-  const newGizi = {
-    ImageUrl: req.file ? `/uploads/${req.file.filename}` : null,
-    Title: req.body.Title,
-    Description: req.body.Description,
-    Nutrient: req.body.Nutrient,
-    Benefit: req.body.Benefit
-  };
-  Gizi.create(newGizi, (err, results)=> {
-    if(err){
-      responseError(res, 500, err.message);
-    }else {
-      responseSucces(res, 'Data gizi berhasil ditambahkan', results);
-    }
-  });
-};
-
-const updateGizi = (req, res)=> {
-  const id = req.params.id;
-  const updateGizi = {
-    ImageUrl: req.file ? `/uploads/${req.file.filename}` : null,
-    Title: req.body.Title,
-    Description: req.body.Description,
-    Nutrient: req.body.Nutrient,
-    Benefit: req.body.Benefit,
-  };
-  Gizi.update(id, updateGizi, (err, results)=> {
+  Gizi.getById(id, (err, result)=> {
     if(err) {
-      responseError(res, 500, err.message);
-    } else{
-      responseSucces(res, `Data gizi dengan ID ${id} berhasil diperbarui`, results);
-    }
-  });
-};
-
-const deleteGizi = (req, res)=> {
-  const id = req.params.id;
-  Gizi.delete(id, (err, results)=> {
-    if(err){
       responseError(err, 500, err.message);
-    }else {
-      responseSucces(res, 'Data gizi dengan ID ${id} berhasil dihapus',results);
+    }else if (!result) {
+      responseError(res, 404, 'Data gizi tidak ditemukan!');
+    } else {
+      responseSucces(res, `Data gizi dengan ID ${id} berhasil ditemukan`, result);
     }
   });
 };
 
 module.exports = {
   getAllGizi,
-  getGiziById,
-  createGizi,
-  updateGizi,
-  deleteGizi
+  getGiziById
 };
--- Backend/src/models/giziModels.js
+++ Backend/src/models/giziModels.js
@@ -1,36 +1,12 @@
-let giziData = [];
+const giziData = require('../data/giziData');
 
 const Gizi = {
-    getAll: (callback) => {
+    getAll: (callback)=> {
         callback(null, giziData);
     },
     getById: (id, callback)=> {
-        const gizi = giziData.find(item => item.id === parseInt(id));
+        const gizi = giziData.find(item=> item.id === parseInt(id));
         callback(null, gizi);
-    },
-    create: (newGizi, callback)=> {
-        newGizi.id = giziData.length ? giziData[giziData.length -1].id + 1 : 1;
-        giziData.push(newGizi);
-        callback(null, newGizi);
-    },
-    update: (id, updateGizi, callback)=> {
-        const index = giziData.findIndex(item => item.id === parseInt(id));
-        if (index === -1) {
-            callback(new Error('Data gizi tidak ditemukan'));
-        } else {
-            updateGizi.id = parseInt(id);
-            giziData[index] = updateGizi;
-            callback(null, updateGizi);
-        }
-    },
-    delete: (id, callback)=> {
-        const index = giziData.findIndex(item => item.id === parseInt(id));
-        if(index === -1){
-            callback(new Error('Data gizi tidak ditemukan'));
-        } else {
-            giziData.splice(index, 1);
-            callback(null, {message: `Data gizi dengan ID ${id} telah dihapus`});
-        }
     }
 };
 module.exports = Gizi;
