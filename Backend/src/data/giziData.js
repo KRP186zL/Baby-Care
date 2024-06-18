930e5233a89820eb63065cf286c675c292ef11c1
Afifrrahman <115561546+Afifrrahman@users.noreply.github.com>
2024-06-18 16:41:36 +0700

    be pembaharuan

---
+++ Backend/src/data/giziData.js
@@ -0,0 +1,94 @@
+const giziData = [
+    {
+        "id": 1,
+        "ImageUrl": "/images/image-9.jpg",
+        "Title": "Apel",
+        "Description": "Apel yang dimasak dan dihaluskan menjadi tekstur lembut. Kaya akan vitamin C, serat, dan antioksidan, apel sangat cocok untuk bayi karena mendukung sistem kekebalan tubuh, baik untuk pencernaan, dan mencegah sembelit.",
+        "Nutrient": "Vitamin C, Serat, Antioksidan",
+        "Benefit": "Mendukung sistem kekebalan tubuh, baik untuk pencernaan, mencegah sembelit"
+    },
+
+    {
+        "id": 2,
+        "ImageUrl": "/images/image-10.jpg",
+        "Title": "Pisang",
+        "Description": "Pisang matang yang dihaluskan, memberikan rasa manis alami dan mudah dicerna. Kaya akan kalium, vitamin B6, vitamin C, dan serat, pisang sangat cocok untuk bayi karena mendukung perkembangan otak, meningkatkan energi, dan baik untuk pencernaan.",
+        "Nutrient": "Kalium, Vitamin B6, Vitamin C, Serat",
+        "Benefit": "Mendukung perkembangan otak, meningkatkan energi, baik untuk pencernaan"
+    },
+
+    {
+        "id": 3,
+        "ImageUrl": "/images/image-1.jpg",
+        "Title": "Wortel",
+        "Description": "Wortel yang dimasak dan dihaluskan menjadi tekstur yang lembut dan manis. Kaya akan vitamin A, beta-karoten, dan serat, wortel sangat cocok untuk bayi karena mendukung kesehatan mata, memperkuat sistem kekebalan tubuh, dan baik untuk pencernaan.",
+        "Nutrient": "Vitamin A, Beta-karoten, Serat",
+        "Benefit": "Mendukung kesehatan mata, memperkuat sistem kekebalan tubuh, baik untuk pencernaan"
+    },
+
+    {
+        "id": 4,
+        "ImageUrl": "/images/image-10.jpg",
+        "Title": "Ubi Jalar",
+        "Description": "Ubi jalar yang dimasak dan dihaluskan menjadi tekstur lembut dan manis. Kaya akan vitamin A, vitamin C, mangan, dan serat, ubi jalar sangat cocok untuk bayi karena mendukung kesehatan mata, meningkatkan sistem kekebalan tubuh, dan baik untuk pencernaan.",
+        "Nutrient": "Vitamin A, Vitamin C, Mangan, Serat",
+        "Benefit": "Mendukung kesehatan mata, meningkatkan sistem kekebalan tubuh, baik untuk pencernaan"
+    },
+
+    {
+        "id": 5,
+        "ImageUrl": "/images/image-3.jpg",
+        "Title": "Pir",
+        "Description": "Buah pir yang dimasak dan dihaluskan menjadi tekstur lembut dan mudah dicerna. Kaya akan vitamin C, serat, dan antioksidan, pir sangat cocok untuk bayi karena mendukung sistem kekebalan tubuh, mencegah sembelit, dan baik untuk pencernaan.",
+        "Nutrient": "Vitamin C, Serat, Antioksidan",
+        "Benefit": "Mendukung sistem kekebalan tubuh, mencegah sembelit, baik untuk pencernaan"
+    },
+
+    {
+        "id": 6,
+        "ImageUrl": "/images/image-4.jpeg",
+        "Title": "Oatmeal",
+        "Description": "Oatmeal yang dihaluskan dan dimasak menjadi tekstur lembut dan mudah dicerna. Kaya akan serat, zat besi, protein, dan vitamin B, oatmeal sangat cocok untuk bayi karena mendukung kesehatan pencernaan, memberikan energi, dan baik untuk perkembangan otak.",
+        "Nutrient": "Serat, Zat Besi, Protein, Vitamin B",
+        "Benefit": "Mendukung kesehatan pencernaan, memberikan energi, baik untuk perkembangan otak"
+    },
+
+    {
+        "id": 7,
+        "ImageUrl": "/images/image-5.jpg",
+        "Title": "Alpukat",
+        "Description": "Alpukat yang dihaluskan menjadi tekstur lembut, kaya akan lemak sehat dan vitamin. Mengandung lemak tak jenuh tunggal, vitamin E, vitamin C, dan vitamin K, alpukat sangat cocok untuk bayi karena mendukung perkembangan otak, baik untuk kulit, dan membantu penyerapan nutrisi lain.",
+        "Nutrient": "Lemak tak jenuh tunggal, Vitamin E, Vitamin C, Vitamin K",
+        "Benefit": "Mendukung perkembangan otak, baik untuk kulit, membantu penyerapan nutrisi lain"
+    },
+
+    {
+        "id": 8,
+        "ImageUrl": "/images/image-6.jpg",
+        "Title": "Labu Kuning",
+        "Description": "Labu kuning yang dimasak dan dihaluskan menjadi tekstur lembut dan manis. Kaya akan vitamin A, vitamin C, kalium, dan serat, labu kuning sangat cocok untuk bayi karena mendukung kesehatan mata, meningkatkan sistem kekebalan tubuh, dan baik untuk pencernaan.",
+        "Nutrient": "Vitamin A, Vitamin C, Kalium, Serat",
+        "Benefit": "Mendukung kesehatan mata, meningkatkan sistem kekebalan tubuh, baik untuk pencernaan"
+    },
+    
+    {
+        "id": 9,
+        "ImageUrl": "/images/image-2.jpg",
+        "Title": "Zucchini",
+        "Description": "Zucchini yang dimasak dan dihaluskan menjadi tekstur lembut dan kaya nutrisi. Mengandung vitamin A, vitamin C, magnesium, dan serat, zucchini sangat cocok untuk bayi karena mendukung kesehatan mata, memperkuat sistem kekebalan tubuh, dan baik untuk pencernaan.",
+        "Nutrient": "Vitamin A, Vitamin C, Magnesium, Serat",
+        "Benefit": "Mendukung kesehatan mata, memperkuat sistem kekebalan tubuh, baik untuk pencernaan"
+    },
+
+    {
+        "id": 10,
+        "ImageUrl": "/images/image-8.jpg",
+        "Title": "Kacang Polong",
+        "Description": "Kacang polong yang dimasak dan dihaluskan menjadi tekstur lembut dan kaya protein. Mengandung protein, vitamin A, vitamin C, dan serat, kacang polong sangat cocok untuk bayi karena mendukung pertumbuhan otot, meningkatkan sistem kekebalan tubuh, dan baik untuk pencernaan.",
+        "Nutrient": "Protein, Vitamin A, Vitamin C, Serat",
+        "Benefit": "Mendukung pertumbuhan otot, meningkatkan sistem kekebalan tubuh, baik untuk pencernaan"
+    } 
+
+
+];
+module.exports = giziData;
