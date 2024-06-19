eb062c2d795a7c66cbc6791f237de1c896102e66
KRP186zL <krisnabonex5743@gmail.com>
2024-06-19 16:09:58 +0700

    Final

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,3 +13,37 @@ const app = new App({
   pengukuranAntropometri: document.querySelector(".pengukuran-antropometri"),
   buttonAntropometri: document.querySelector("#buttonAntropometri"),
 });
+
+// MENAMPILKAN GIZI
+const getGizidata = async () => {
+  try {
+    const response = await fetch(
+      "https://innate-garnet-aunt.glitch.me/api/gizi"
+    );
+    const Apigizi = await response.json();
+    let giziElement = "";
+    Apigizi.data.forEach((gizi) => {
+      // const imageUrl = `https://restaurant-api.dicoding.dev/images/medium/${gizi.pictureId}`;
+
+      giziElement += `
+      <div class="gizi_card">
+                        <div class="gizi_image">
+                            <img class="foto" src="${gizi.ImageUrl}" alt="${gizi.Title}" crossorigin="anonymous" loading="lazy">
+                        </div>
+                        <div class="gizi_card_content">
+                            <h4>${gizi.Title}</h4>
+                        </div>
+                        <p>
+                        ${gizi.Description}
+                        </p>
+                    </div>
+      `;
+    });
+
+    document.querySelector(".gizi_grid").innerHTML = giziElement;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getGizidata();
